5baee35f0a30022ae50295e832df920d311baa24
tilakjain619 <frontendchamp123@gmail.com>
2023-09-02 18:36:53 +0530

    update

---
+++ Readme.md
@@ -0,0 +1,66 @@
+# Dizcuss - A Discussion Forum
+
+Welcome to the **Dizcuss - A Discussion Forum** GitHub repository! This repository contains the source code for an interactive and user-friendly feedback board discussion forum built using HTML, CSS, JavaScript, Node.js, Express.js, and MongoDB.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+## Introduction
+Dizcuss is a web application that allows users to engage in discussions and provide feedback on various topics. This platform is designed to facilitate easy communication and collaboration among users. It's built using a combination of frontend and backend technologies, providing a seamless experience for users to share their thoughts.
+
+## Features
+- **User Authentication:** Users can create accounts, log in, and log out securely.
+- **Discussion Threads:** Users can initiate and participate in discussion threads on different topics.
+- **Feedback Board:** Dedicated section for users to share feedback and suggestions.
+- **Responsive Design:** The interface is responsive, ensuring optimal user experience across devices.
+- **Database Integration:** MongoDB is used to store user data, discussions, and feedback securely.
+
+## Installation
+1. Clone this repository to your local machine using:
+   ```
+   git clone https://github.com/tilakjain619/dizcuss.git
+   ```
+
+2. Navigate to the project directory:
+   ```
+   cd dizcuss
+   ```
+
+3. Install the required dependencies using:
+   ```
+   npm install
+   ```
+
+4. Create a `.env` file in the root directory and provide your MongoDB connection string:
+   ```
+   MONGODB_URI=your-mongodb-connection-string
+   ```
+
+## Usage
+1. Start the server:
+   ```
+   npm start
+   ```
+
+2. Open your web browser and visit `http://localhost:3000` to access the Dizcuss application.
+
+3. Register an account or log in to an existing account.
+
+4. Explore discussion threads, contribute to discussions, and provide feedback on the feedback board.
+
+## Contributing
+Contributions are welcome! If you'd like to contribute to the project, please follow these steps:
+1. Fork the repository.
+2. Create a new branch for your feature or improvement.
+3. Make your changes and commit them.
+4. Push your changes to your fork.
+5. Create a pull request describing your changes.
+
+
+---
+
+Thank you for your interest in **Dizcuss - A Discussion Forum**! If you encounter any issues or have suggestions, please feel free to [create an issue](https://github.com/tilakjain619/dizcuss/issues). We look forward to your contributions ⚡
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
-const multer = require('multer');
 const LocalStrategy = require('passport-local').Strategy;
 // const User = require('../models/user');
 const passportLocalMongoose = require('passport-local-mongoose');
@@ -30,19 +29,14 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const storage = multer.diskStorage({
-  destination: './public/uploads/profile', // Upload directory
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-
-const upload = multer({ storage });
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  profileImage: String,
+  likedDiscussions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Discussion' }],
+  dislikedDiscussions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Discussion' }],
+  likedReplies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }],
+  dislikedReplies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }],
   discussions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Discussion' }],
   replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }]
 });
@@ -69,13 +63,10 @@ app.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-app.post('/signup', upload.single('profileImage'), async (req, res) => {
+app.post('/signup', async (req, res) => {
   try {
     const { username, password } = req.body;
-    const profileImageURL = '/uploads/profile/' + req.file.filename; // Define profileImageURL
-    const profileImage = req.file ? profileImageURL : ''; // Use profileImageURL here
-
-    const user = new User({ username, profileImage });
+    const user = new User({username});
     await User.register(user, password);
 
     res.redirect('/login'); // Redirect to login page after successful signup
@@ -91,7 +82,7 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', passport.authenticate('local', {
-  successRedirect: '/profile',
+  successRedirect: '/home',
   failureRedirect: '/login'
 }));
 
@@ -127,7 +118,6 @@ app.post('/update-profile', isLoggedIn, async (req, res) => {
 // Define Mongoose schema and models
 const discussionSchema = new mongoose.Schema({
   content: String,
-  profileImage: String,
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
@@ -136,7 +126,6 @@ const discussionSchema = new mongoose.Schema({
 
 const replySchema = new mongoose.Schema({
   content: String,
-  profileImage: String,
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
@@ -153,7 +142,7 @@ mongoose.connect(mongoURI, {
     console.log('Connected to MongoDB Database');
 
     // Create a new discussion and save it to the database
-    app.post('/api/discussions', isLoggedIn, upload.single('profileImage'), async (req, res) => {
+    app.post('/api/discussions', isLoggedIn, async (req, res) => {
       try {
         const newDiscussion = new Discussion({
           content: req.body.content,
@@ -186,7 +175,7 @@ mongoose.connect(mongoURI, {
       }
     });
 
-    app.post('/api/discussions/:discussionId/replies', isLoggedIn, upload.single('profileImage'), async (req, res) => {
+    app.post('/api/discussions/:discussionId/replies', isLoggedIn, async (req, res) => {
       try {
         const discussion = await Discussion.findById(req.params.discussionId);
         if (!discussion) {
@@ -232,65 +221,103 @@ mongoose.connect(mongoURI, {
     // Route for handling likes for discussion
     app.put('/api/likes/:discussionId', isLoggedIn, async (req, res) => {
       try {
-          const discussion = await Discussion.findById(req.params.discussionId);
-  
-          if (!discussion) {
-              return res.status(404).json({ message: 'Discussion not found' });
-          }
-  
-          const updatedDiscussion = await Discussion.findByIdAndUpdate(
-              req.params.discussionId,
-              { $inc: { likes: 1 } }, // Increment the likes count by 1
-              { new: true } // Return the updated discussion
-          );
-  
-          res.json(updatedDiscussion);
-      } catch (error) {
-          res.status(500).json({ message: 'Error updating likes' });
-      }
-  });
-    // Route for handling dislikes for discussion
-    app.put('/api/dislikes/:discussionId', isLoggedIn, async (req, res) => {
-      try {
-          const discussion = await Discussion.findById(req.params.discussionId);
-  
-          if (!discussion) {
-              return res.status(404).json({ message: 'Discussion not found' });
-          }
-  
-          const updatedDiscussion = await Discussion.findByIdAndUpdate(
-              req.params.discussionId,
-              { $inc: { dislikes: 1 } }, // Increment the likes count by 1
-              { new: true } // Return the updated discussion
-          );
-  
-          res.json(updatedDiscussion);
-      } catch (error) {
-          res.status(500).json({ message: 'Error updating likes' });
-      }
-  });
-  
-
-   // Route for handling likes for replies
-    app.put('/api/likes/:replyId', isLoggedIn, async (req, res) => {
-      try {
-        const reply = await Reply.findById(req.params.replyId);
+        const discussion = await Discussion.findById(req.params.discussionId);
     
-        if (!reply) {
-          return res.status(404).json({ message: 'Reply not found' });
+        if (!discussion) {
+          return res.status(404).json({ message: 'Discussion not found' });
         }
     
-        const updatedReply = await Reply.findByIdAndUpdate(
-          req.params.replyId,
-          { $inc: { likes: 1 } }, // Increment the likes count by 1
-          { new: true } // Return the updated reply
-        );
+        const user = req.user;
     
-        res.json(updatedReply);
+        // Check if the user has already liked this discussion
+        if (user.likedDiscussions.includes(discussion._id)) {
+          // If yes, remove the like
+          user.likedDiscussions.pull(discussion._id);
+          discussion.likes--;
+        } else {
+          // If no, add the like
+          user.likedDiscussions.push(discussion._id);
+          discussion.likes++;
+        }
+    
+        // Save the changes
+        await user.save();
+        await discussion.save();
+    
+        res.json(discussion);
       } catch (error) {
         res.status(500).json({ message: 'Error updating likes' });
       }
     });
+    
+    // Route for handling dislikes for discussion
+    app.put('/api/dislikes/:discussionId', isLoggedIn, async (req, res) => {
+      try {
+        const discussion = await Discussion.findById(req.params.discussionId);
+    
+        if (!discussion) {
+          return res.status(404).json({ message: 'Discussion not found' });
+        }
+    
+        const user = req.user;
+    
+        // Check if the user has already disliked this discussion
+        if (user.dislikedDiscussions.includes(discussion._id)) {
+          // If yes, remove the dislike
+          user.dislikedDiscussions.pull(discussion._id);
+          discussion.dislikes--;
+        } else {
+          // If no, add the dislike
+          user.dislikedDiscussions.push(discussion._id);
+          discussion.dislikes++;
+        }
+    
+        // Save the changes
+        await user.save();
+        await discussion.save();
+    
+        res.json(discussion);
+      } catch (error) {
+        res.status(500).json({ message: 'Error updating dislikes' });
+      }
+    });
+    
+  
+
+   // Route for handling likes for replies
+   app.put('/api/likes/:replyId', isLoggedIn, async (req, res) => {
+    try {
+      const reply = await Reply.findById(req.params.replyId);
+  
+      if (!reply) {
+        return res.status(404).json({ message: 'Reply not found' });
+      }
+  
+      const user = req.user;
+  
+      // Check if the user has already liked this reply
+      if (user.likedReplies.includes(reply._id)) {
+        // If yes, remove the like
+        user.likedReplies.pull(reply._id);
+        reply.likes--;
+      } else {
+        // If no, add the like
+        user.likedReplies.push(reply._id);
+        reply.likes++;
+      }
+  
+      // Save the changes
+      await user.save();
+      await reply.save();
+  
+      res.json(reply);
+    } catch (error) {
+      res.status(500).json({ message: 'Error updating likes' });
+    }
+  });
+  
+  // Similar route for disliking replies
+  
     // Route for handling dislikes for replies
     app.put('/api/dislikes/:replyId', isLoggedIn, async (req, res) => {
       try {
